13532cd0f606b248a03a9c6c0adab109c4f0a586
Mathew <matymcleod@gmail.com>
2021-05-09 20:16:08 +0000

    adding packing-list and index.html

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,20 @@
+//---------- LHL - Iterating over Arrays
+//----- Using a for loop
+/*
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+
+//----- Using a while loop
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
